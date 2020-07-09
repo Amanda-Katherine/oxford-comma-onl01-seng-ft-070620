998b4289c432a1641b067a17f7c5cdf7b7735259
Amanda Walker <walker.akw@gmail.com>
2020-07-09 18:42:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,11 @@ def oxford_comma(array)
   else
     oxford_array = []
     array.each_with_index do |index, final|
+<<<<<<< HEAD
       if final == array.size - 1
+=======
+      if index == array.size - 1
+>>>>>>> b1fd6a435eb40fc5e3bd1de3b80d51ba59c9a7cd
         oxford_array.push("and #{index}")
       else
         oxford_array.push("#{index}, ")
@@ -17,6 +21,9 @@ def oxford_comma(array)
     oxford_array.join
   end #if
 end #oxford_comma
+<<<<<<< HEAD
 
 print oxford_comma(["fiddleheads","okra","kohlrabi", 1, 2, 3, 4, 5, 6, 7])
 #returns fiddleheads, okra, kohlrabi, 1, 2, 3, 4, 5, 6, and 7
+=======
+>>>>>>> b1fd6a435eb40fc5e3bd1de3b80d51ba59c9a7cd
